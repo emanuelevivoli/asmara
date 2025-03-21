0e9d9bd99b62f73cf521c69745b53b6593f99551
Jacopown <jacopomehi@gmail.com>
2025-03-21 19:02:23 +0100

    pytorch dataset rework

---
+++ dataset/HoloMineDataset.py
@@ -0,0 +1,98 @@
+import sys
+import os
+import torch
+from torch.nn import functional as F
+
+import numpy as np
+import pandas as pd
+
+from pathlib import Path
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from scripts.utils.data import task_manager
+
+class HoloMineDataset(torch.utils.data.Dataset):
+
+    def __init__(
+        self,
+        classes: int = 1,
+        interps: bool = True,
+        inversion: bool = False,
+        real_out: bool = True,
+    ) -> None:
+
+        self.classes = classes
+        self.interps = interps
+        self.inversion = inversion
+        self.real_out = real_out
+
+        data_path = Path(__file__).parent.resolve() / "data/processed_data"
+        if self.interps:
+            data_path = data_path / "interpolated"
+        else:
+            data_path = data_path / "standard"
+
+        self.meta_path = data_path / "meta"
+
+        if self.inversion:
+            self.data_path = data_path / "inversions"
+        else:
+            self.data_path = data_path / "holograms"
+
+        self.csv = pd.read_csv(self.meta_path / "mix.csv")
+
+    def __getitem__(self, index):
+        row = self.csv.iloc[index]
+        suffix = "_inv" if self.inversion else ""
+        data = np.load(
+            self.data_path / f"{row['mix_name']}{suffix}.npy", allow_pickle=True
+        )
+        data = torch.from_numpy(data).cfloat()
+
+        # data.shape should be torch.Size([52, 62])
+        if not self.interps and (
+            data.shape[0] != 52 or data.shape[1] != 62
+        ):
+
+            real_data = torch.view_as_real(data)  # [52, 61]
+            real_data = real_data.permute(2, 0, 1).unsqueeze(0)  # [1, 2, 52, 61]
+            real_data = F.interpolate(
+                real_data, size=(52, 62), mode="nearest"
+            )  # [1, 2, 52, 62]
+            real_data = (
+                real_data.squeeze(0).permute(1, 2, 0).contiguous()
+            )  # [52, 62, 2]
+            data = torch.view_as_complex(real_data)  # [52, 62]
+
+        label = task_manager(self.classes, row)
+
+        if self.real_out:
+            mix = torch.real(data).unsqueeze(0)
+            mix = torch.nan_to_num(mix)
+        else:
+            mix = data.unsqueeze(0)
+
+        mix = mix.permute(*torch.arange(mix.ndim - 1, -1, -1))
+
+        if not self.interps:
+            if mix.shape[0] != 62:
+                raise ValueError(f"mix.shape[0] != 62, but {mix.shape[0]}")
+            if mix.shape[1] != 52:
+                raise ValueError(f"mix.shape[1] != 52, but {mix.shape[1]}")
+            if mix.shape[2] != 1:
+                raise ValueError(f"mix.shape[2] != 1, but {mix.shape[2]}")
+
+        # reshape the mix to have channels first
+        if not self.inversion:
+            mix = mix.permute(2, 0, 1)
+        else:
+            mix = mix.permute(3, 2, 0, 1)
+
+        return mix.float(), label
+
+    def __sizeof__(self) -> int:
+        return len(self.csv)
+
+    def __len__(self) -> int:
+        return len(self.csv)
